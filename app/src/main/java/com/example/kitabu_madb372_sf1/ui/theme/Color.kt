f5ff8d297449d16593cee2e62b24bc009f8b0238
clevegoosen <goosencleve@gmail.com>
2026-09-04 16:15:17 +0200

    Add app colors

---
+++ app/src/main/java/com/example/kitabu_madb372_sf1/ui/theme/Color.kt
@@ -0,0 +1,21 @@
+package com.example.kitabu_madb372_sf1.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val KitabuPurple = Color(0xFF6C4AB6)
+val KitabuTeal = Color(0xFF00897B)
+val KitabuCoral = Color(0xFFE76F51)
+val KitabuBackground = Color(0xFFF9F7FC)
+
+val AvailableGreen = Color(0xFF00897B)
+val BorrowedRed = Color(0xFFD32F2F)
+val WarningOrange = Color(0xFFE76F51)
+
+// Default Compose colors (kept for compatibility if needed, though we'll use Kitabu ones)
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
